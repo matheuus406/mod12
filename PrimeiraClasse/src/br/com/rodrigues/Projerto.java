2cae4ec386dbaa2e58d06ac283e27dfbfb289779
matheus406 <matheussrodriguess406@gmail.com>
2023-11-11 20:35:54 -0300

    Tarefa

---
+++ PrimeiraClasse/src/br/com/rodrigues/Projerto.java
@@ -0,0 +1,23 @@
+package br.com.rodrigues;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Projerto {
+	public static void main(String[] args) {
+		String[] lista = new String[7];
+		lista[0] = "Matheus sexo m";
+		lista[1] = "Roberta sexo f";
+		lista[2] = "Julia sexo f";
+		lista[3] = "Gustavo sexo m";
+		lista[4] = "thiago sexo m";
+		lista[5] = "Manoel sexo m";
+		
+		System.out.println("lista Masculina: "+ lista[0] + " : " + lista[3] + " : " +lista[4] + " : " +lista[5]);
+		
+		
+		System.out.println("lista feminina: " + lista[1] + " : " +lista[2] );
+}
+
+}
